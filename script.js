f7f7d85dfe2e4bf0c907791991b52ca952787f7d
dhruvgoel1234 <02619011621.aiml@gmail.com>
2023-10-25 20:29:40 +0530

    Tailwind Files Uploaded

---
+++ script.js
@@ -0,0 +1,15 @@
+// Toggling of Bars and Cross
+const bars = document.getElementById('bars');
+const cross = document.getElementById('cross');
+const menu = document.getElementById('menu');
+
+bars.addEventListener('click',() => {
+    bars.classList.toggle('hidden');
+    cross.classList.toggle('hidden');
+    menu.classList.toggle('scale-y-0');
+})
+cross.addEventListener('click',() => {
+    cross.classList.toggle('hidden');
+    bars.classList.toggle('hidden');
+    menu.classList.toggle('scale-y-0');
+})
